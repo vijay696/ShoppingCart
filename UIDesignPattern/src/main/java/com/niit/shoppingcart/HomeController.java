86bd1dbc5f78a50b04353e7cf18da87f1295dbf5
Vijay G R <vijay.gr11@gmail.com>
2016-08-23 21:19:17 +0530

    UI design front end
    basic landing page design

---
+++ UIDesignPattern/src/main/java/com/niit/shoppingcart/HomeController.java
@@ -0,0 +1,44 @@
+package com.niit.shoppingcart;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.servlet.ModelAndView;
+
+@Controller
+public class HomeController {
+
+	//if you want to navigate
+	@RequestMapping("/")
+	public String home()
+	{
+		return "Index";
+	}
+}
+	
+	/*
+	@RequestMapping("/")
+	public ModelAndView home()
+	{
+		ModelAndView mv = new ModelAndView("/Home");
+		mv.addObject("message","thank you for visiting");
+		return mv;
+	}
+	
+	@RequestMapping("/Register")
+	public ModelAndView register()
+	{
+		ModelAndView mv = new ModelAndView("Register");
+		mv.addObject("user click register here", "true");
+		return mv;
+	}
+	
+	@RequestMapping("/Login")
+	public ModelAndView login()
+	{
+	
+		ModelAndView mv = new ModelAndView("Login");
+		mv.addObject("user click login here", "true");
+		return mv;
+	}
+}
+*/
---
+++ UIDesignPattern/target/ShoppingCartFrontEnd/WEB-INF/classes/com/niit/shoppingcart/HomeContoller.java
@@ -0,0 +1,5 @@
+package com.niit.shoppingcart;
+
+public class HomeContoller {
+
+}
